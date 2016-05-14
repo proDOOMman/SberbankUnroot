132cee7a77bee39f82b44949a604bd6355ffbd13
proDOOMman <proDOOMman@gmail.com>
2016-05-14 12:19:42 +0300

    Add information about SberbankUnroot module to Sberbank online GUI

--- app/src/main/java/com/prodoomman/sberbankunroot/Unroot.java
+++ app/src/main/java/com/prodoomman/sberbankunroot/Unroot.java
@@ -4,6 +4,7 @@ import android.content.pm.PackageInfo;
 
 import java.lang.reflect.Method;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
@@ -13,7 +14,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedHelpers.findClass;
 
-public class Unroot implements IXposedHookLoadPackage {
+public class Unroot implements IXposedHookLoadPackage, IXposedHookInitPackageResources {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if(!lpparam.packageName.startsWith("ru.sberbankmobile")) {
@@ -103,4 +104,14 @@ public class Unroot implements IXposedHookLoadPackage {
 
     }
 
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
+        if(!resparam.packageName.startsWith("ru.sberbankmobile")) {
+            return;
+        }
+        resparam.res.setReplacement(resparam.packageName, "string", "user_id_support",
+                "№ %s. Это номер вашего устройства. Сообщите его оператору контактного-центра при необходимости.\n" +
+                        "Не рекомендуется сообщать операторам про root и xposed.\n" +
+                        "Используется модуль SberbankUnroot " + BuildConfig.VERSION_NAME + ".");
+    }
 }
