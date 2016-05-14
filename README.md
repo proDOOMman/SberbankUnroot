7cce9934e49ba1ec01daa2c894c7f19bcd890a66
proDOOMman <proDOOMman@gmail.com>
2016-05-14 12:15:38 +0300

    Initial release v1.0.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SberbankUnroot
 Xposed module for disabling root detection in Sberbank online app
+
+http://repo.xposed.info/module/com.prodoomman.sberbankunroot
---
+++ app/src/main/java/com/prodoomman/sberbankunroot/Unroot.java
@@ -0,0 +1,57 @@
+package com.prodoomman.sberbankunroot;
+
+import android.content.pm.PackageInfo;
+
+import java.lang.reflect.Method;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findClass;
+
+public class Unroot implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if(!lpparam.packageName.startsWith("ru.sberbankmobile")) {
+            return;
+        }
+
+        try {
+
+            Class<?> rootDetector = findClass("com.kavsdk.rootdetector.RootDetector", lpparam.classLoader);
+
+            try {
+
+                XposedHelpers.findAndHookMethod(rootDetector, "checkRoot", new XC_MethodReplacement() {
+
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                        return false;
+                    }
+                });
+            } catch (Exception e) {
+                XposedBridge.log(e);
+            }
+
+            try {
+                XposedHelpers.findAndHookMethod(rootDetector, "isRoot", new XC_MethodReplacement() {
+
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                        return false;
+                    }
+                });
+            } catch (Exception e) {
+                XposedBridge.log(e);
+            }
+
+        }
+        catch(XposedHelpers.ClassNotFoundError e) {
+            XposedBridge.log(e);
+        }
+
+}
